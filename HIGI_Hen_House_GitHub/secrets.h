d5e4c718eaeb35554321f75e4e89fb0b81d42fcb
retirement2021 <95634398+retirement2021@users.noreply.github.com>
2026-05-21 12:22:56 +0100

    Delete HIGI_Hen_House_GitHub directory

--- HIGI_Hen_House_GitHub/secrets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// WiFi Credentials
-#define WIFI_SSID "YourWiFi"
-#define WIFI_PASSWORD "YourPassword"
-
-// Location of the Hen House
-double latitude = 50.00000;
-double longitude = -3.00000;
