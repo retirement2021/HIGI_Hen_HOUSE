aed26cc390e3703839c45d84f97297e6faef3a49
retirement2021 <95634398+retirement2021@users.noreply.github.com>
2026-05-29 16:16:15 +0100

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// WiFi Credentials
+#define WIFI_SSID "your internet"
+#define WIFI_PASSWORD "Your password"
+
+// Location of Hen House
+double latitude = 50.0000;
+double longitude = -3.0000;
