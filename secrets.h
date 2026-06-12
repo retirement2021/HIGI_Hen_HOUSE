7f0973c907f1e64986ef21617addddddd61f2187
retirement2021 <95634398+retirement2021@users.noreply.github.com>
2026-06-12 11:29:09 +0100

    Delete secrets.h

--- secrets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// WiFi Credentials
-#define WIFI_SSID "Your-WiFi"
-#define WIFI_PASSWORD "Your-Password"
-
-// Location of Hen House
-double latitude = 50.0000;
-double longitude = -3.0000;
