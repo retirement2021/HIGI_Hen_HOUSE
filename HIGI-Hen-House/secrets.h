920259f595785e386508d8675cc7aaff8b3715f9
retirement2021 <95634398+retirement2021@users.noreply.github.com>
2026-06-12 11:30:57 +0100

    Delete HIGI-Hen-House directory

--- HIGI-Hen-House/secrets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// WiFi Credentials
-#define WIFI_SSID "Your WiFi"
-#define WIFI_PASSWORD "Your PassWord"
-
-// Location of Hen House, for sunrise & sunset times.
-double latitude = 50.0000;
-double longitude = -3.0000;
