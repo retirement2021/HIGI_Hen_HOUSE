6c47fbacb5e2ed1cf6adb9f393a5e439aa17c957
retirement2021 <95634398+retirement2021@users.noreply.github.com>
2026-06-12 11:31:34 +0100

    Add files via upload

---
+++ HIGI-Hen-House/secrets.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// WiFi Credentials
+#define WIFI_SSID "Your WiFi"
+#define WIFI_PASSWORD "Your PassWord"
+
+// Location of Hen House, for sunrise & sunset times.
+double latitude = 50.0000;
+double longitude = -3.0000;
